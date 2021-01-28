b5e599a69d6a8832a1545aa636066b0cfbc11ede
ayou0076 <77496817+ayou0076@users.noreply.github.com>
2021-01-27 23:43:56 -0500

    Add files via upload

---
+++ Mohammad-Ayoub-printing-output-gh-pages/js/index.js
@@ -0,0 +1,45 @@
+
+
+let a = 3;
+let b = 5;
+let c;
+
+let value; 
+
+
+value = 'let a = 3;\n';
+value += 'let b = 5;\n';
+value += 'let c;\n';
+value += '--------------------\n';
+
+
+value += 'a + b = ' + (a + b) + '\n';
+value += 'a - b = ' + (a - b) + '\n';
+value += 'a * b = ' + (a * b) + '\n';
+value += 'a / b = ' + (a / b) + '\n';
+value += 'a % b = ' + (a % b) + '\n';
+value += 'a += b = ' + (a += b) + '\n';
+value += 'a -= b = ' + (a -= b) + '\n';
+value += 'a *= b = ' + (a *= b) + '\n';
+value += 'a /= b = ' + (a /= b) + '\n';
+value += 'a %= b = ' + (a %= b) + '\n';
+value += 'a == b : ' + (a == b) + '\n';
+value += 'a != b : ' + (a != b) + '\n';
+value += 'a > b : ' + (a > b) + '\n';
+value += 'a < b : ' + (a < b) + '\n';
+value += '!a && !c : ' + (!a && !c) + '\n';
+value += '!a || !c : ' + (!a || !c) + '\n';
+alert(value);
+
+
+
+let firstName = 'Mohammad';
+let lastName = 'Ayoub';
+let email = 'ayou0076@algonquinlive.com';
+
+let output;
+
+output = `My name is ${firstName} ${lastName}. You can contact me at ${email}`;
+
+alert(output);
+
